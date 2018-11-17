c4efaa3b1327251f96de26ce0f89d27a79ffc3cb
Jeff Wright <jeffw387@gmail.com>
2018-11-16 22:29:37 -0800

    formatting

--- msdfgen.h
+++ msdfgen.h
@@ -9,9 +9,9 @@
  * The technique used to generate multi-channel distance fields in this code
  * has been developed by Viktor Chlumsky in 2014 for his master's thesis,
  * "Shape Decomposition for Multi-Channel Distance Fields". It provides improved
- * quality of sharp corners in glyphs and other 2D shapes in comparison to monochrome
- * distance fields. To reconstruct an image of the shape, apply the median of three
- * operation on the triplet of sampled distance field values.
+ * quality of sharp corners in glyphs and other 2D shapes in comparison to
+ * monochrome distance fields. To reconstruct an image of the shape, apply the
+ * median of three operation on the triplet of sampled distance field values.
  *
  */
 
@@ -29,17 +29,51 @@
 namespace msdfgen {
 
 /// Generates a conventional single-channel signed distance field.
-void generateSDF(Bitmap<float> &output, const Shape &shape, double range, const Vector2 &scale, const Vector2 &translate);
+void generateSDF(
+    Bitmap<float> &output,
+    const Shape &shape,
+    double range,
+    const Vector2 &scale,
+    const Vector2 &translate);
 
 /// Generates a single-channel signed pseudo-distance field.
-void generatePseudoSDF(Bitmap<float> &output, const Shape &shape, double range, const Vector2 &scale, const Vector2 &translate);
+void generatePseudoSDF(
+    Bitmap<float> &output,
+    const Shape &shape,
+    double range,
+    const Vector2 &scale,
+    const Vector2 &translate);
 
-/// Generates a multi-channel signed distance field. Edge colors must be assigned first! (see edgeColoringSimple)
-void generateMSDF(Bitmap<FloatRGB> &output, const Shape &shape, double range, const Vector2 &scale, const Vector2 &translate, double edgeThreshold = 1.00000001);
+/// Generates a multi-channel signed distance field. Edge colors must be
+/// assigned first! (see edgeColoringSimple)
+void generateMSDF(
+    Bitmap<FloatRGB> &output,
+    const Shape &shape,
+    double range,
+    const Vector2 &scale,
+    const Vector2 &translate,
+    double edgeThreshold = 1.00000001);
 
-// Original simpler versions of the previous functions, which work well under normal circumstances, but cannot deal with overlapping contours.
-void generateSDF_legacy(Bitmap<float> &output, const Shape &shape, double range, const Vector2 &scale, const Vector2 &translate);
-void generatePseudoSDF_legacy(Bitmap<float> &output, const Shape &shape, double range, const Vector2 &scale, const Vector2 &translate);
-void generateMSDF_legacy(Bitmap<FloatRGB> &output, const Shape &shape, double range, const Vector2 &scale, const Vector2 &translate, double edgeThreshold = 1.00000001);
+// Original simpler versions of the previous functions, which work well under
+// normal circumstances, but cannot deal with overlapping contours.
+void generateSDF_legacy(
+    Bitmap<float> &output,
+    const Shape &shape,
+    double range,
+    const Vector2 &scale,
+    const Vector2 &translate);
+void generatePseudoSDF_legacy(
+    Bitmap<float> &output,
+    const Shape &shape,
+    double range,
+    const Vector2 &scale,
+    const Vector2 &translate);
+void generateMSDF_legacy(
+    Bitmap<FloatRGB> &output,
+    const Shape &shape,
+    double range,
+    const Vector2 &scale,
+    const Vector2 &translate,
+    double edgeThreshold = 1.00000001);
 
-}
+}  // namespace msdfgen
